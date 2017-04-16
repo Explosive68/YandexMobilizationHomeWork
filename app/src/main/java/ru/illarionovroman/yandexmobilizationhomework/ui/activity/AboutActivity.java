9699d69235d257a9c2bca18b8e8befc1e2389e9f
Explosive68 <illroman@yandex.ru>
2017-04-16 18:08:43 +0700

    Implement SettingsFragment. Add AboutActivity with custom logo and text.

---
+++ app/src/main/java/ru/illarionovroman/yandexmobilizationhomework/ui/activity/AboutActivity.java
@@ -0,0 +1,46 @@
+package ru.illarionovroman.yandexmobilizationhomework.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import butterknife.ButterKnife;
+import ru.illarionovroman.yandexmobilizationhomework.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        ButterKnife.bind(this);
+        initializeActivity();
+    }
+
+    private void initializeActivity() {
+        initializeActionBar();
+    }
+
+    private void initializeActionBar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarAbout);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle(R.string.about_title);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
--- app/src/main/java/ru/illarionovroman/yandexmobilizationhomework/ui/fragment/SettingsFragment.java
+++ app/src/main/java/ru/illarionovroman/yandexmobilizationhomework/ui/fragment/SettingsFragment.java
@@ -1,20 +1,22 @@
 package ru.illarionovroman.yandexmobilizationhomework.ui.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.LinearLayout;
 
 import butterknife.BindView;
 import ru.illarionovroman.yandexmobilizationhomework.R;
+import ru.illarionovroman.yandexmobilizationhomework.ui.activity.AboutActivity;
 
 
 public class SettingsFragment extends BaseFragment {
 
-    @BindView(R.id.tvSettings)
-    TextView mTvSettings;
+    @BindView(R.id.llAbout)
+    LinearLayout mLlAbout;
 
     public SettingsFragment() {
     }
@@ -34,5 +36,10 @@ public class SettingsFragment extends BaseFragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        mLlAbout.setOnClickListener(itemView -> {
+            Intent aboutIntent = new Intent(getContext(), AboutActivity.class);
+            startActivity(aboutIntent);
+        });
     }
 }
